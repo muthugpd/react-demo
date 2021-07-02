b037fce8a36a2c9372234aeccba93b02cd56c009
Srivastan <52730816+srivastan@users.noreply.github.com>
2021-07-02 11:57:45 +0530

    React demo app

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'demo-app'
+import 'demo-app/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
